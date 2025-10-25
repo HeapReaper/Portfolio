bfc15c93c70f6247ffa8c0dafc43dcdb85371136
HeapReaper <kelvin@aerobytes.nl>
2025-10-25 21:14:49 +0200

    Added images, updated favicon and site metadata

--- src/components/sections/Hero.tsx
+++ src/components/sections/Hero.tsx
@@ -6,20 +6,20 @@ import { RiNextjsFill } from "react-icons/ri";
 
 export default function HeroSection() {
   return (
-    <div className="relative flex flex-col justify-center items-center h-screen p-6 bg-gray-900 overflow-hidden space-y-5">
+    <div className="relative flex flex-col justify-center items-center h-screen p-6  overflow-hidden space-y-5">
       {/* Animated icons in background */}
       <FaReact className="animated-icon speed-1 text-blue-500" style={{ top: '10%', left: '20%' }} />
       <RiNextjsFill className="animated-icon speed-2 text-orange-500" style={{ top: '50%', left: '10%' }} />
       <FaDocker className="animated-icon speed-3 text-pink-500" style={{ top: '30%', left: '90%' }} />
       <SiMariadb className="animated-icon speed-1 text-red-500" style={{ top: '80%', left: '30%' }} />
-      <FaLinux className="animated-icon speed-3 text-green-500" style={{ top: '90%', left: '80%' }} />
+      <FaLinux className="animated-icon speed-2 text-green-500" style={{ top: '90%', left: '80%' }} />
 
       {/* Hero content */}
       <Image
-        src="https://placehold.co/140x140/png"
+        src="/me-160x160.webp"
         className="rounded-full border border-purple-700 shadow-[0_0_40px_rgba(168,85,247,0.6)] transition-transform hover:scale-110 duration-300"
-        width={140}
-        height={140}
+        width={160}
+        height={160}
         alt="Profile picture"
       />
 
