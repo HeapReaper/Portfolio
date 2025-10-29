b4ce7a5d079d5648c61d03573ff3f85300ecdd7a
HeapReaper <kelvin@aerobytes.nl>
2025-10-29 21:10:54 +0100

    Updated project text

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -1,17 +1,17 @@
 export const projectsData = [
   {
     title: "Orbit",
-    description: "Sed ut perspiciatis unde omnis iste natus error sit voluptatem.",
+    description: "An extensive dashboard to configure the multi server Discord bot.",
     url: "https://botinorbit.com",
     architecture: [
-      "Frontend: React (NextJS) with TailwindCSS",
-      "Backend: NextJS 15.5.5",
-      "Database: MariaDB with Redis and ClickHouse",
-      "Other: Using Docker with CD"
+      "Frontend: React (NextJS) with TailwindCSS.",
+      "Backend: NextJS 15.5.5.",
+      "Database: MariaDB with Redis and ClickHouse.",
+      "Other: Using Docker with CD."
     ],
     achievements: [
       "Built and deployed the full stack application independently.",
-      "Implemented key feature X using technology Y.",
+      "Using Redis caching to minimise database reads and to prevent Discord API limits.",
       "Improved API security by using sessions, rate limiting, "
     ],
     imageUrl: "/Orbit-screenshot.png",
@@ -22,10 +22,10 @@ export const projectsData = [
     description: "The completely free CV generator",
     url: "https://klikcv.nl",
     architecture: [
-      "Frontend: React (NextJS) with TailwindCSS",
-      "Backend: NextJS 15.5.5",
-      "Database: DB-less",
-      "Other: Using Docker with CD"
+      "Frontend: React (NextJS) with TailwindCSS.",
+      "Backend: NextJS 15.5.5.",
+      "Database: DB-less.",
+      "Other: Using Docker with CD."
     ],
     achievements: [
       "Built and deployed the full stack application independently.",
@@ -39,15 +39,15 @@ export const projectsData = [
     description: "Sed ut perspiciatis unde omnis iste natus error sit voluptatem.",
     url: "",
     architecture: [
-      "Frontend: Bootstrap with Livewire",
-      "Backend: Laravel 11.9 with queues",
-      "Database: MariaDB with Redis",
-      "Other: Using Docker with CD",
+      "Frontend: Bootstrap with Livewire.",
+      "Backend: Laravel 11.9 with queues.",
+      "Database: MariaDB with Redis.",
+      "Other: Using Docker with CD.",
     ],
     achievements: [
-      "Handled and reported 1500+ flights from 50+ members",
-      "Uses Queues to process PDF generation in the background",
-      "Optimized the frontend to be accessible to elderly members"
+      "Handled and reported 1500+ flights from 50+ members.",
+      "Using Queues to process PDF generation in the background.",
+      "Optimized the frontend to be accessible to elderly members."
     ],
     imageUrl: "/kiyomi-screenshot.png",
   },
